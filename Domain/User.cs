7d54833e87ec0719bbac01e1c5cd100a88bbb20c
Erick Clavell <clavellerick@gmail.com>
2021-09-18 14:51:52 -0300

    Projeto criado

---
+++ Domain/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Domain
+{
+    public class User: IIdentityEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        private ICollection<TaskToDo> _taskToDo { get; set; }
+
+        public virtual IReadOnlyCollection<TaskToDo> TasksToDo { get { return _taskToDo as Collection<TaskToDo>;  } }
+
+        public User()
+        {
+            this._taskToDo = new Collection<TaskToDo>();
+        }
+
+        public void AddItemToDo(TaskToDo taskToDo)
+        {
+            _taskToDo.Add(taskToDo);
+        }
+    }
+}
