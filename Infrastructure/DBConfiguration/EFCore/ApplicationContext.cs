1e59665a7b26326daa3756b72f845dbc7e8d2361
Erick Clavell <clavellerick@gmail.com>
2021-09-18 15:46:42 -0300

    First Code Entity SQLServer

---
+++ Infrastructure/DBConfiguration/EFCore/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.DBConfiguration.EFCore
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext() { }
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            if (!dbContextOptionsBuilder.IsConfigured)
+            {
+                dbContextOptionsBuilder.UseSqlServer(DatabaseConnection.ConnectionConfiguration.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> User { get; set; }
+        public DbSet<TaskToDo> TaskToDo { get; set; }
+    }
+}
